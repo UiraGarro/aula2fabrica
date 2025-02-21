4e1de55e0817902f146d30396b2259e8580c370c
UiraGarro <extadexavier@gmail.com>
2025-02-21 14:47:46 -0300

    coiso3

---
+++ coiso 3.py
@@ -0,0 +1,12 @@
+for i in range(5): #repete 5 vezes (de 0 a 4)>>> conta ate x, sendo range(x)
+    print("Repetição número: ", i) 
+
+counter = 0
+max = 10
+while counter <= max -1:
+    counter += 1
+    prin(counter)
+    
+names = ['Pedro', 'Joao', 'Leticia']
+for name in names:
+    print(name)
