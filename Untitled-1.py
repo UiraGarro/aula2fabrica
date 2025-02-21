bc63c5dea56e2cc3a4c151235302fc91478d3d81
UiraGarro <extadexavier@gmail.com>
2025-02-21 14:37:44 -0300

    cu
    cu

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+#Tipos de Variáveis
+
+nome = "Roudgi" #string (frase/sequencia de caracteres)
+idade = 100000000 #integer (numero inteiro)
+altura = 5.42 #float (numero real)
+obeso = True #boolian (verdade ou mentirada)
+ 
+print(nome, type(nome))
+print(idade, type(idade))
+print(altura, type(altura))
+print(obeso, type(obeso))
